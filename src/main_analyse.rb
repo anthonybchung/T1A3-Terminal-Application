c446eed621f315c57c45f2a25ab076d02ae87726
anthonybchung <11941@coderacademy.edu.au>
2022-04-23 14:37:24 +1000

    analsysis formula and table display

--- src/main_analyse.rb
+++ src/main_analyse.rb
@@ -21,7 +21,7 @@ module MainAnalyse
       # get initial initial index
       initial_end_number = invoice_struct_array.length - 1
       begin
-        puts "Enter the initial index [1-#{initial_end_number}]"
+        puts "Enter the starting index [1-#{initial_end_number}]"
         initial_index = gets.chomp
         if /\d+$/.match? initial_index
           initial_int = initial_index.to_i
@@ -37,7 +37,7 @@ module MainAnalyse
 
       # get final index
 begin
-        puts "Enter the final index [#{initial_int+1}-#{invoice_struct_array.length}]"
+        puts "Enter the ending index [#{initial_int+1}-#{invoice_struct_array.length}]"
         final_index = gets.chomp
         if /\d+$/.match? final_index
           final_int = final_index.to_i
@@ -53,27 +53,70 @@ begin
     data_index_0 = initial_int - 1
     data_index_1 = final_int - 1
     analyse_array = invoice_struct_array[data_index_0..data_index_1]
-    analyse_last = analyse_array.length-1
     
-    date_difference = Date.parse(analyse_array[analyse_last].purchase_date).mjd - Date.parse(analyse_array[0].purchase_date).mjd
+    date_difference = Date.parse(analyse_array[-1].purchase_date).mjd - Date.parse(analyse_array[0].purchase_date).mjd
      
     # Cost for the past week
     total_paid = 0
-    total_distance = analyse_array[analyse_last].odometer - analyse_array[0].odometer
+    total_distance = analyse_array[-1].odometer - analyse_array[0].odometer
     total_fuel_qty = 0
-puts analyse_array
+
     analyse_array.each do |item|
         total_paid += item.paid.to_f
        
         total_fuel_qty += item.fuel_qty.to_f
     end
       # average cost per km
-      net_paid = total_paid - analyse_array[0].paid.to_f
-      net_fuel_qty = total_fuel_qty - analyse_array[0].fuel_qty.to_f
-      cost_per_day = net_paid / date_difference.to_f
-      puts "total paid: #{net_paid}"
-      puts "number of days: #{date_difference}"
-    puts cost_per_day
+      # Cost spent or amount of fuel consumed during this period
+      consume_array = analyse_array.drop(1)
+      consume_array.each do |item|
+        total_paid += item.paid.to_f
+        total_fuel_qty += item.fuel_qty.to_f
+    end
+
+    #$/km
+    money_per_km = total_paid / total_distance
+
+    litre_per_km = total_fuel_qty / total_distance
+
+    #distance travelled
+    total_distance = analyse_array[-1].odometer.to_f - analyse_array[0].odometer.to_f
+
+      table_header = ['Description'.colorize(:blue),'Result'.colorize(:blue)]
+      table_body = []
+
+      table_line = []
+      table_line << "Starting Date:"
+      table_line << analyse_array[0].purchase_date
+      table_body << table_line
+
+      table_line = []
+      table_line << "End Date:".colorize(:blue)
+      table_line << analyse_array[-1].purchase_date.colorize(:blue)
+      table_body << table_line
+
+      table_line =[]
+      table_line << "Money spent during this time"
+      table_line << "$#{total_paid.round(2)}"
+    table_body << table_line
+      table_line =[]
+      table_line << "Distance travelled during this time".colorize(:blue)
+     table_line << "#{total_distance}km".colorize(:blue)
+     table_body << table_line
+
+     table_line = []
+     table_line<< "Cost per km"
+     table_line << "$#{money_per_km.round(2)}/km"
+     table_body << table_line
+
+     table_line = []
+     table_line << "Litre per km".colorize(:blue)
+     table_line << "#{litre_per_km.round(2)}litre/km".colorize(:blue)
+     table_body << table_line
+    
+      table = TTY::Table.new(table_header,table_body)
+      puts table.render :unicode
+     
       # average km per weeek
 
       # average km per L
